9b8ff2439eca2af5f29ece502d50706718e478c8
sonicdcer <alejandro.asenjo88@gmail.com>
2023-05-04 02:15:24 -0300

    EntityBlueAxeKnight Decompiled

--- src/st/top/28C38.c
+++ src/st/top/28C38.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "stage.h"
 
 INCLUDE_ASM("config/../asm/us/st/top/nonmatchings/28C38", func_801A8C38);
 
@@ -256,7 +257,398 @@ INCLUDE_ASM("config/../asm/us/st/top/nonmatchings/28C38", func_801B8DC0);
 
 INCLUDE_ASM("config/../asm/us/st/top/nonmatchings/28C38", func_801B95C4);
 
+// assembler skips a nop
+// matching in decompme: https://decomp.me/scratch/7Aat2
+#ifndef NON_MATCHING
 INCLUDE_ASM("config/../asm/us/st/top/nonmatchings/28C38", func_801B96A8);
+#else
+extern s32 D_80180A18;
+extern s32 D_801819BC;
+extern s32 D_801819C4;
+extern s16 D_801819D4[];
+extern s16 D_801819D6[]; 
+extern u8 D_801819F4[]; // Animation:
+extern u8 D_80181A04[]; // Animation:
+extern u8 D_80181A1C[]; // Animation:
+extern u8 D_80181A30[]; // Animation:
+extern u8 D_80181A3C[]; // Animation:
+extern u8 D_80181A4C[]; // Animation:
+extern u8 D_80181A5C[]; // Animation:
+extern u8 D_80181A70[]; // Animation:
+extern u8 D_80181A80[]; // Animation:
+extern u8 D_80181A98[]; // Animation:
+extern u8 D_80181AA4[]; // Animation:
+extern u8 D_80181AB0[]; // Animation:
+extern u8 D_80181AC0[]; // Animation:
+
+typedef enum {
+    AXE_KNIGHT_INIT,
+    AXE_KNIGHT_IDLE,
+    AXE_KNIGHT_2,
+    AXE_KNIGHT_3,
+    AXE_KNIGHT_4,
+    AXE_KNIGHT_5,
+    AXE_KNIGHT_6,
+    AXE_KNIGHT_7,
+    AXE_KNIGHT_8,
+} BlueAxeKnightSteps;
+
+s32 func_801B0B78(const u8 frames[], Entity* entity);
+// EntityBlueAxeKnight
+void func_801B96A8(Entity* self) {
+    Entity* newEntity;
+    s32 animStatus;
+    s16 temp_s2;
+    s32 temp_a0;
+    s8* hitbox;
+    s32 i;
+
+    if ((self->hitPoints < 8) && ((u8) self->unk7C.S8.unk1 != 0)) {
+        self->unk7C.S8.unk1 = 0;
+        self->animCurFrame += 0x17;
+        newEntity = func_801B12B8(D_8007D858, &D_8007D858[32]);
+        if (newEntity != NULL) {
+            for (i = 0; i < 2; i++) {
+                func_801AFD1C(2, self, newEntity);
+                if (self->facing != 0) {
+                    newEntity->posX.i.hi += 12;
+                } else {
+                    newEntity->posX.i.hi -= 12;
+                }
+                newEntity->subId = 2;
+                newEntity->posY.i.hi -=  8 - (i * 8);
+            }
+        }
+    }
+    
+    if (self->flags & 0x100) {
+        if (self->step != 8) {
+            func_801B6B0C(0x767);
+            self->unk3C = 0;
+            self->unk80.modeS16.unk0 = 0x41;
+            self->zPriority -= 0x10;
+            func_801B1688(8);
+            if (func_801AD630() & 1) {
+                self->unk2E = 0x10;
+                self->unk80.modeS16.unk0 = 0;
+            } else {
+                self->unk2E = 0;
+            }
+            
+        }
+    }
+    
+    switch (self->step) {
+    case AXE_KNIGHT_INIT:
+        func_801B1758(&D_80180A18);
+        self->facing = (func_801B0DB8() & 1) ^ 1;
+        self->unk12 = 10;
+        if (self->subId != 0) {
+            self->palette += 2;
+        }
+        self->unk7C.S8.unk1 = 1;
+        self->unk80.modeS16.unk2 = 0x200;
+        func_801AFD1C(0x2A, self, &self[1]);
+        break;
+        
+    case AXE_KNIGHT_IDLE:
+        if (func_801B0E58(&D_801819C4) & 1) {
+            self->facing = (func_801B0DB8() & 1) ^ 1;
+            func_801B1688(AXE_KNIGHT_2);
+        }
+        break;
+        
+    case AXE_KNIGHT_2:
+        if (self->unk2E == 0) {
+            if (self->facing == 0) {
+                self->accelerationX = -0x5000;
+            } else {
+                self->accelerationX = 0x5000;
+            }
+            self->unk2E++;
+        }
+        
+        if (self->unk7C.U8.unk1 != 0) {
+            animStatus = func_801B0B78(D_801819F4, self);
+        } else {
+            animStatus = func_801B0B78(D_80181A4C, self);
+        }
+        if (!(animStatus & 0xFF)) {
+            self->facing = (func_801B0DB8() & 1) ^ 1;
+            
+            if (self->facing == 0) {
+                self->accelerationX = -0x5000;
+            } else {
+                self->accelerationX = 0x5000;
+            }
+            if (func_801B0D48() < 96) {
+                func_801B1688(AXE_KNIGHT_3);
+                self->unk7C.S8.unk0 = 1;
+            }
+        }
+        
+        if ((self->animFrameIdx == 1) || (self->animFrameIdx == 4)) {
+            if (self->facing == 0) {
+                self->accelerationX -= 0x600;
+            } else {
+                self->accelerationX += 0x600;
+            }
+        } else if (self->facing != 0) {
+            self->accelerationX -= 0x600;
+        } else {
+            self->accelerationX += 0x600;
+        }
+        
+        if (func_801B10D0(&D_801819BC) & 0x60) {
+            self->posX.val -= self->accelerationX;
+            self->accelerationX = 0;
+        }
+        func_801B95C4();
+        break;
+        
+    case AXE_KNIGHT_3:
+        if (self->unk2E == 0) {
+            if (self->facing == 0) {
+                self->accelerationX = 0x6000;
+            } else {
+                self->accelerationX = -0x6000;
+            }
+            self->unk2E++;
+        }
+        if ((u8) self->unk7C.S8.unk1 != 0) {
+            animStatus = func_801B0B78(D_801819F4, self);
+        } else {
+            animStatus = func_801B0B78(D_80181A4C, self);
+        }
+        
+        if (!(animStatus & 0xFF)) {
+            self->facing = (func_801B0DB8() & 1) ^ 1;
+            if (self->facing == 0) {
+                self->accelerationX = 0x6000;
+            } else {
+                self->accelerationX = -0x6000;
+            }
+
+            if (func_801B0D48() >= 0x51) {
+                func_801B1688(AXE_KNIGHT_2);
+                self->unk7C.S8.unk0 = 0;
+            }
+        }
+        if ((self->animFrameIdx == 1) || (self->animFrameIdx == 4)) {
+            if (self->facing != 0) {
+                self->accelerationX  -= 0x400;
+            } else {
+                self->accelerationX  += 0x400;
+            }
+        } else if (self->facing != 0) {
+            self->accelerationX  += 0x400;
+        } else {
+            self->accelerationX  -= 0x400;
+        }
+        if (func_801B10D0(&D_801819BC) & 0x60) {
+            self->posX.val -= self->accelerationX;
+            self->accelerationX = 0;
+        }
+        func_801B95C4();
+        break;
+        
+    case AXE_KNIGHT_4:
+        if (self->unk7C.U8.unk1 != 0) {
+            animStatus = func_801B0B78(D_80181A04, self);
+        } else {
+            animStatus = func_801B0B78(D_80181A80, self);
+        }
+        
+        if (!(animStatus & 0xFF)) {
+block_63:
+            if (func_801B0D48() < 89) {
+                func_801B1688(AXE_KNIGHT_3);
+                self->unk7C.S8.unk0 = 1;
+            } else {
+                func_801B1688(AXE_KNIGHT_2);
+                self->unk7C.S8.unk0 = 0;
+            }
+        } else if ((animStatus & 0x80) && (self->animFrameIdx == 7)) {
+            func_801B6B0C(0x766);
+            func_801B6B0C(0x6C7);
+            newEntity = func_801B12B8(D_8007A958, &D_8007A958[32]);
+            if (newEntity != NULL) {
+                func_801AFCA8(0x29, newEntity);
+                newEntity->facing = self->facing;
+                newEntity->posY.i.hi -= 12;
+                if (newEntity->facing != 0) {
+                    newEntity->posX.i.hi += 8;break;
+                } 
+                newEntity->posX.i.hi -= 8;
+            }
+        }
+        break;
+        
+    case AXE_KNIGHT_5:
+        if (self->unk7C.U8.unk1 != 0) {
+            animStatus = func_801B0B78(D_80181A1C, self);
+        } else {
+            animStatus = func_801B0B78(D_80181A5C, self);
+        }
+        if (animStatus & 0xFF) {
+            if ((animStatus & 0x80) && (self->animFrameIdx == 6)) {
+                func_801B6B0C(0x766);
+                func_801B6B0C(0x6C7);
+                newEntity = func_801B12B8(D_8007A958, &D_8007A958[32]);
+                if (newEntity != NULL) {
+                    func_801AFCA8(0x29, newEntity);
+                    newEntity->facing = self->facing;
+                    newEntity->posY.i.hi += 12;
+                    newEntity->subId = 1;
+                    if (newEntity->facing != 0) {
+                        newEntity->posX.i.hi += 8;break;
+                    }
+                    newEntity->posX.i.hi -= 8;
+                }
+            }
+        } else {
+            goto block_63;
+        }
+        break;
+        
+    case AXE_KNIGHT_6:
+        switch (self->unk2E) {
+        case 0:               
+            self->unk80.modeS16.unk0 = 0x20;
+            self->animCurFrame = 4;
+            self->unk2E++;
+            break;
+            
+        case 1:               
+            if (self->unk80.modeS16.unk0 != 0) {
+                self->unk80.modeS16.unk0--;
+            } else {
+                self->unk80.modeS16.unk0 = 0x40;
+                if (self->facing != 0) {
+                    self->accelerationX = 0x20000 | 0x8000;
+                }else {
+                    self->accelerationX = 0xFFFD0000 | 0x8000;
+                }
+                self->unk2E++;
+                
+            }
+            break;
+            
+        case 2:
+            if (self->unk7C.U8.unk1 != 0) {
+                func_801B0B78(D_80181A30, self);
+            } else {
+                func_801B0B78(D_80181A98, self);
+            }
+            
+            if ((self->unk80.modeS16.unk0 == 0) || (func_801B0D48() < 0x20)) {
+                func_801B6B0C(0x766);
+                self->animFrameIdx = 0;
+                self->animFrameDuration = 0;
+                self->unk2E++;
+            } else {
+                self->unk80.modeS16.unk0--;
+            }
+            
+            if (func_801B10D0(&D_801819BC) & 0x60) {
+                self->posX.val -= self->accelerationX;
+                self->accelerationX = 0;
+            }
+            break;
+            
+        case 3:
+            if (self->unk7C.U8.unk1 != 0) {
+                animStatus = func_801B0B78(D_80181A3C, self);
+            } else {
+                animStatus = func_801B0B78(D_80181A70, self);
+            }
+            if (!(animStatus & 0xFF)) {
+                self->facing = (func_801B0DB8() & 1) ^ 1;
+                goto block_63;
+            }
+            
+            if (self->accelerationX != 0) {
+                if (self->accelerationX < 0) {
+                    self->accelerationX += 0x4000;
+                } else {
+                    self->accelerationX -= 0x4000;
+                }
+                if (func_801B10D0(&D_801819BC) & 0x60) {
+                self->posX.val -= self->accelerationX;
+                self->accelerationX = 0;
+                }
+            }
+        }
+        break;
+        
+    case AXE_KNIGHT_7:
+        if (self->unk7C.U8.unk1 != 0) {
+            animStatus = func_801B0B78(D_80181A04, self);
+        } else {
+            animStatus = func_801B0B78(D_80181A80, self);
+        }
+        if (!(animStatus & 0xFF)) {
+            if (func_801B0D48() > 88) {
+                func_801B1688(AXE_KNIGHT_2);
+                self->unk7C.S8.unk0 = 0;break;
+            } else {
+                func_801B1688(AXE_KNIGHT_3);
+                self->unk7C.S8.unk0 = 1;break;
+            }
+        }
+        
+        if ((animStatus & 0x80) && (self->animFrameIdx == 7)) {
+            func_801B6B0C(0x766);
+            func_801B6B0C(0x6C7);
+            newEntity = func_801B12B8(D_8007A958, &D_8007A958[32]);
+            if (newEntity != NULL) {
+                func_801AFCA8(0x29, newEntity);
+                newEntity->facing = self->facing;
+                newEntity->posY.i.hi -= 0x28;
+                newEntity->subId = 2;
+                if (newEntity->facing != 0) {
+                    newEntity->posX.i.hi += 16;
+                } else {
+                    newEntity->posX.i.hi -= 16;
+                }
+            }
+        }
+        break;
+        
+    case AXE_KNIGHT_8:
+        if (self->unk80.modeS16.unk0 != 0) {
+            temp_s2 = self->unk80.modeS16.unk0 - 1;
+            self->unk80.modeS16.unk0 = temp_s2;
+            if (!(temp_s2 & 7)) {
+                newEntity = func_801B12B8(D_8007D858, &D_8007D858[32]);
+                if (newEntity != NULL) {
+                    func_801AFD1C(2, self, newEntity);
+                    temp_a0 = ((s32) (temp_s2 << 0x10) >> 0x13) * 2;
+                    newEntity->subId = 2;
+                    newEntity->posX.i.hi += D_801819D4[temp_a0];
+                    newEntity->posY.i.hi += D_801819D6[temp_a0];
+                }
+            }
+        }
+        if (func_801B0B78(D_80181AA4, self) == 0) {
+            if (func_801B8DC0(self) != 0) {
+                func_801B0A48(self);
+                return;
+            }
+            if (self->unk2E < 16) {
+                self->animCurFrame = 0;
+            }
+        }
+        break;
+    }
+    hitbox = 4 * D_80181AC0[self->animCurFrame] + D_80181AB0 - 1;
+    hitbox++;
+    self->unk10 = *hitbox++;
+    self->unk12 = *hitbox++;
+    self->hitboxWidth = hitbox[0];
+    self->hitboxHeight = hitbox[1];
+}
+#endif
 
 INCLUDE_ASM("config/../asm/us/st/top/nonmatchings/28C38", func_801BA0EC);
 
